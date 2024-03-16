223293b546abb6ab7d574784dc1aad7e335054dc
Elettra Tegon <112578860+neTegon@users.noreply.github.com>
2024-03-16 15:18:06 +0100

    Add files via upload

---
+++ tegon.elettra.4i.Gelati/Ingrediente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.Gelati
+{
+    public class Ingrediente
+    {
+        private int idGelati { get; set; }
+        private string Tipo { get; set; }
+        private string Valore { get; set; }
+        public Ingredienti(string riga)
+        {
+            string[] campi = riga.Split(';');
+            this.idGelati = Convert.ToInt32(campi[0]);
+            this.Tipo = campi[1];
+            this.Valore = campi[2];
+        }
+    }
+}
---
+++ tegon.elettra.4i.Gelati/Ingredienti.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.Gelati
+{
+    public class Ingredienti
+    {
+        private List<Ingrediente> list;
+        public Ingredienti(string nomeFile)
+        {
+            StreamReader sr = new StreamReader(nomeFile);
+            while (sr.EndOfStream)
+            {
+
+                list.Add(new Ingrediente(sr.ReadLine()));
+            }
+        }
+
+    }
+}
