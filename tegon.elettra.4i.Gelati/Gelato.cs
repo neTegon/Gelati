605365f2310184740d087279f930f2ca65196e4e
Elettra Tegon <112578860+neTegon@users.noreply.github.com>
2024-03-20 09:08:54 +0100

    Add files via upload

---
+++ tegon.elettra.4i.Gelati/Gelato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.Gelati
+{
+    public class Gelato
+    {
+        private int id { get; set; }
+        private string name { get; set; }
+        private string description { get; set; }
+        private double prezzo { get; set; }
+        public Gelato(string riga)
+        {
+            string[] campi = riga.Split(';');
+            this.id = Convert.ToInt32(campi[0]);
+            this.name = campi[1];
+            this.description = campi[2];
+            this.prezzo = Convert.ToDouble(campi[3]);
+        }
+    }
+}
---
+++ tegon.elettra.4i.Gelati/Ingrediente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.Gelati
+{
+    public class Ingrediente
+    {
+        private int idGelati { get; set; }
+        private string Tipo { get; set; }
+        private string Valore { get; set; }
+        
+        
+        public Ingrediente(string riga)
+        {
+            string[] campi = riga.Split(';');
+            this.idGelati = Convert.ToInt32(campi[0]);
+            this.Tipo = campi[1];
+            this.Valore = campi[2];
+        }
+    }
+    public class lattosio:Ingrediente
+    {
+        private string lat {  get; set; }
+        public lattosio(string riga):base(riga) 
+        {
+            string[] campi = riga.Split(';');
+            this.lat = campi[2];
+        }
+    }
+}
