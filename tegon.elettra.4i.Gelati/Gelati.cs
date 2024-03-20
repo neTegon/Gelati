6071ab0c042547bcfb65a3d6a5a8671460f9d2e8
Elettra Tegon <112578860+neTegon@users.noreply.github.com>
2024-03-20 08:21:45 +0100

    Add files via upload

---
+++ tegon.elettra.4i.Gelati/Gelati.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.Gelati
+{
+    public class Gelati
+    {
+       private List<Gelato> list;
+       public Gelati(string nomeFile)
+        {
+            StreamReader sr = new StreamReader(nomeFile);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                
+                list.Add(new Gelato(sr.ReadLine()));
+            }
+       }
+    }
+}
---
+++ tegon.elettra.4i.Gelati/Ingrediente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.Gelati
+{
+    public class Ingrediente
+    {
+        private int idGelati { get; set; }
+        private string Tipo { get; set; }
+        private string Valore { get; set; }
+        private string calorie { get; set; }
+        private string lattosio { get; set; }
+        private string colorante { get; set; }
+        public Ingrediente(string riga)
+        {
+            string[] campi = riga.Split(';');
+            this.idGelati = Convert.ToInt32(campi[0]);
+            this.Tipo = campi[1];
+            this.Valore = campi[2];
+            if(Tipo=="colorante")
+            {
+                this.colorante = campi[3];
+            }
+            else if(Tipo=="Panna")
+            {
+                this.calorie = campi[3];
+            }
+            else if(Tipo=="Latte")
+            {
+                this.lattosio = campi[3];
+            }
+        }
+    }
+}
